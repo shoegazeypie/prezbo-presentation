1ee8e4bc480f2ab74439b57bfecc92cada725da2
OpenShift guest <clarkson.rosie@gmail.com>
2013-04-16 17:00:17 -0400

    working catch up method

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = require('http').createServer(handler)
 , fs = require('fs')
 , fu = require("./fu")
 
-//app.listen(process.env.PORT || 8001);
-app.listen(1337);
+app.listen(process.env.PORT || 8001);
+//app.listen(1337);
 
 
 var clients = [ ];
