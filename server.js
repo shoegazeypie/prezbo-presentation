6feee00b0fd138793f88f4e0f18b92601725eaa2
OpenShift guest <andy.4705@gmail.com>
2013-04-18 06:32:15 -0400

    server handler rewrite

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 var app = require('http').createServer(handler)
 , io = require('socket.io').listen(app)
 , fs = require('fs')
-, fu = require("./fu")
+, crypto = require('crypto')
+, PATH = require('path');
 
 app.listen(process.env.PORT || 1337);
-//app.listen(1337);
+
+    
+var MIME_TYPES = {
+
+    '.txt': 'text/plain',
+	'.md': 'text/plain',
+	'': 'text/plain',
+	'.html': 'text/html',
+	'.css': 'text/css',
+	'.js': 'application/javascript',
+	'.json': 'application/json',
+	'.jpg': 'image/jpeg',
+	'.png': 'image/png',
+	'.gif': 'image/gif'
+
+};
+	
+var CONFIG = {
+	
+	'site_base': '.',
+	
+	'file_expiry_time': 480, // HTTP cache expiry time, minutes
+	
+	'directory_listing': true
+
+};
+
+var EXPIRY_TIME = (CONFIG.file_expiry_time * 60).toString();
 
 
 var clients = [ ];
@@ -25,58 +53,109 @@ for (var i=0; i < options.length; i++) {
 
 
 function handler (req, res) {
-   //remove query params
-   q = req.url.indexOf('?');
-   if(q != -1){root = req.url.substring(0, q);}
-   else{root=req.url;}
-	 //root = req.url;
-
-		console.log("trying to load " + root);
-	 
-    if (root != "/" && root!="/controller"){
- 
-        var filename = root.replace("/","");
- 
-        var content_type = fu.mime.lookupExtension(fu.extname(filename));
- 
-        fs.readFile(filename, function(err, data) {
-            if (err) {
-                   
-                console.log("Error loading " + filename);
-            }
-            else {
-                var body = String(data);
-               
-                //body = body.replace("##PORT##",process.env.PORT);
-                //body = body.replace("##IP##",process.env.IP);
-                var headers = {
-                    "Content-Type": content_type,
-                    "Content-Length": body.length
-                };
-                //if (!DEBUG) headers["Cache-Control"] = "public";
-                
-                res.writeHead(200, headers);
-                /*if(headers['Content-Type'] == 'image/gif'){
-                    res.end(data, 'binary');
-                }
-                else{*/
-                    res.end(req.method === "HEAD" ? "" : body);
-                //}
-            }
-        });
-    }else{   
-        fs.readFile('index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
-           
-            res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
-            res.end(data);
-        });
-    }
+    var headers;
+    var etag;
+	
+	if ( req.method === 'GET' ){
+//		Get response object
+		handleRequest( req.url, function ( response_object ){
+			if ( response_object.data && response_object.data.length > 0 ){
+				etag = response_object.getEtag();
+				if ( req.headers.hasOwnProperty('if-none-match') && req.headers['if-none-match'] === etag ){
+//					Not Modified
+					res.writeHead( 304 );
+					res.end();
+				}
+				else {
+					headers = {
+						'Content-Type': response_object.type,
+						'Content-Length' : response_object.data.length,
+						'Cache-Control' : 'max-age=' + EXPIRY_TIME,
+						'ETag' : etag
+					};
+					res.writeHead( response_object.status, headers );
+					res.end( response_object.data );
+				}
+			}
+			else {
+				res.writeHead( response_object.status );
+				res.end();
+			}
+		} );
+	}
+	else {
+//		Forbidden
+		res.writeHead( 403 );
+		res.end();
+	}
 }
+
+// Filter server requests by type
+
+function handleRequest( url, callback ){
+    console.log("handleRequest " + url);
+    if ( PATH.extname( url ) === '' ){
+		getFileResponse( '/index.html', function ( response_object ){
+			callback( response_object );
+		} );
+	}
+	else {
+		getFileResponse( url, function ( response_object ){
+			callback( response_object );
+		} );
+	}
+
+}
+
+// Creates a ResponseObject from a local file path
+
+function getFileResponse( path, callback ){
+
+    
+
+//    console.log("getFileResponse " + path);
+  path = CONFIG.site_base + path;
+
+    console.log("getFileResponse " + path);
+
+	fs.exists( path, function ( path_exists ){
+		if ( path_exists ){
+            console.log("exists");
+			fs.readFile( path, function ( error, data ){
+				if ( error ){
+//					Internal error
+					callback( new ResponseObject( {'data': error.stack, 'status': 500} ) );
+				}
+				else {
+					callback( new ResponseObject( {'data': new Buffer( data ), 'type': MIME_TYPES[PATH.extname(path)]} ) );
+				}
+			} );
+		}
+		else {
+            console.log("not found");
+//			Not found
+			callback( new ResponseObject( {'status': 404} ) );
+		}
+	} );
+
+}
+
+// An object representing a server response
+
+function ResponseObject( metadata ){
+
+    this.status = metadata.status || 200;
+	this.data = metadata.data || false;
+	this.type = metadata.type || false;
+
+}
+
+ResponseObject.prototype.getEtag = function (){
+    var hash = crypto.createHash( 'md5' );
+	hash.update( this.data );
+	return hash.digest( 'hex' );
+};
+
 
 
 io.sockets.on('connection', function (socket) {
